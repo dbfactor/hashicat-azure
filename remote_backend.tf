d50c4fba10b326082b87e34ac0739e3f8b682460
dbfactor <coruscent@gmail.com>
2021-08-23 01:52:58 +0000

    Added the first tag

--- remote_backend.tf
+++ remote_backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "david-boughton-training"
     workspaces {
       name = "hashicat-azure"
